93af087ad87b5417e147f634f92f126feaa11240
titohajili <maqatoto963@gmail.com>
2025-01-07 20:12:28 +0400

    JS route configuration

--- assets/js/route.js
+++ assets/js/route.js
@@ -3,8 +3,16 @@ import {updateWeather, error404} from "./api.js";
 const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474";
 
 const currentLocation = function (){
+    window.navigator.geolocation.getCurrentPosition(res => {
+        const {latitude, longitude} = res.coords;
 
+        updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+    }, err => {
+        window.location.hash = defaultLocation;
+    });
 }
+
+const searchedLocation = query => updateWeather(...query.split("&"));
 
 const routes = new Map([
     ["/current-location", currentLocation],
@@ -12,7 +20,11 @@ const routes = new Map([
 ]);
 
 const chekHash = function () {
-    
+    const requestURL = window.location.hash.slice(1);
+
+    const [route, query] = requestURL.includes ? requestURL.split("?") : [requestURL]
+
+    routes.get(route)? routes.get(route)(query) : error404();
 }
 
 window.addEventListener("load", function (){
@@ -21,4 +33,4 @@ window.addEventListener("load", function (){
     }else{
         chekHash();
     }
-})
+});
