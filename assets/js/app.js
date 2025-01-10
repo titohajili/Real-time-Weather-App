c8f861e1f26aa126a5fa7687bf75a6d9c4d2a1e2
titohajili <maqatoto963@gmail.com>
2025-01-10 23:26:23 +0400

    Search toggle api

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import { fetchData, url } from "./api.js";
+import * as module from "./module.js";
+
+
+
+const addEventOnElements = function (elements, eventType, callback){
+    for (const element of elements) element.addEventListener(eventType, callback);
+}
+
+
+
+const searchView = document.querySelector("[data-search-view]");
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+
+
+const toggleSearch = () => serarchViev.classList.toggle("active");
+addEventOnElements(searchTogglers, "click", toggleSearch);
